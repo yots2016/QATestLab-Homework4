831c6b561c9b807ed3255bc5ebeb0eb315c1419f
Viacheslav Otamas <newstageja@gmail.com>
2018-06-13 10:07:45 +0300

    init commit

---
+++ src/test/java/myprojects/automation/assignment4/tests/CreateProductTest.java
@@ -0,0 +1,17 @@
+package myprojects.automation.assignment4.tests;
+
+import myprojects.automation.assignment4.BaseTest;
+import org.testng.annotations.Test;
+
+public class CreateProductTest extends BaseTest {
+
+    @Test
+    public void createNewProduct(String login, String password) {
+        // TODO implement test for product creation
+
+        // actions.login(login, password);
+        // ...
+    }
+
+    // TODO implement logic to check product visibility on website
+}
