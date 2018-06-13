ee9ebbf921c4fa26464e62e1de556cfd4b9d91a0
Viacheslav Otamas <newstageja@gmail.com>
2018-06-13 13:21:07 +0300

    edited GeneralActions class

--- src/test/java/myprojects/automation/assignment4/GeneralActions.java
+++ src/test/java/myprojects/automation/assignment4/GeneralActions.java
@@ -2,7 +2,11 @@ package myprojects.automation.assignment4;
 
 
 import myprojects.automation.assignment4.model.ProductData;
+import myprojects.automation.assignment4.utils.Properties;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 /**
@@ -24,7 +28,18 @@ public class GeneralActions {
      */
     public void login(String login, String password) {
         // TODO implement logging in to Admin Panel
-        throw new UnsupportedOperationException();
+//        throw new UnsupportedOperationException();
+
+        driver.get(Properties.getBaseAdminUrl());
+
+        WebElement emailWebElement = driver.findElement(By.id("email"));
+        emailWebElement.sendKeys(login);
+
+        WebElement passwordWebElement = driver.findElement(By.id("passwd"));
+        passwordWebElement.sendKeys(password);
+
+        WebElement loginButtonWebElement = driver.findElement(By.name("submitLogin"));
+        loginButtonWebElement.click();
     }
 
     public void createProduct(ProductData newProduct) {
@@ -34,11 +49,14 @@ public class GeneralActions {
 
     /**
      * Waits until page loader disappears from the page
+     * @param by
      */
-    public void waitForContentLoad() {
+    public WebElement waitForContentLoad(By by) {
         // TODO implement generic method to wait until page content is loaded
 
         // wait.until(...);
         // ...
+
+        return wait.until(ExpectedConditions.presenceOfElementLocated(by));
     }
 }
